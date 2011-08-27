a21c0eaed7efd0148b7835456eae0aca7c39abec
Mike Ivanov <mike@mikeivanov.com>
2011-08-27 15:07:02 -0700

    update the "database" from twitter

--- server.py
+++ server.py
@@ -3,16 +3,17 @@
 import re
 import os
 import sys
-import time
 import bottle
 import logging
-import urllib2
+import twitter
 import datetime
-import contextlib
+import threading
 
 from bottle import route
 
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+
+UPDATE_INTERVAL = 10.0
 
 logging.basicConfig()
 log = logging.getLogger('vanshelter')
@@ -21,6 +22,7 @@ log.setLevel(logging.DEBUG)
 def now():
     return datetime.datetime.now().isoformat()
 
+#XXX: make it come from an yaml file
 # our "database"
 data = [dict(id="bcdata_bridge",
              name="Bridge Women's Emergency Shelter",
@@ -32,6 +34,7 @@ data = [dict(id="bcdata_bridge",
              capacity=12,
              available=0,
              last_updated=now(),
+             last_id=None,
              kind="Women",
              notes=None),
 
@@ -45,6 +48,7 @@ data = [dict(id="bcdata_bridge",
              capacity=32,
              available=0,
              last_updated=now(),
+             last_id=None,
              kind="Women and their Children",
              notes=None),
 
@@ -58,9 +62,30 @@ data = [dict(id="bcdata_bridge",
              capacity=46,
              available=0,
              last_updated=now(),
+             last_id=None,
              kind="Men and Women",
              notes=None),
         ]
+
+tapi = twitter.Api()
+
+availability_rx = re.compile(r'\[(\d+)/(\d+)\]')
+def update_shelter_data(shelter):
+    statuces = tapi.GetUserTimeline(screen_name="mike_ivanov",
+                                    since_id=shelter['last_id'])
+    if len(statuces) > 0:
+        last_id = statuces[0].id
+        for status in reversed(statuces):
+            match = availability_rx.search(status.text)
+            if match:
+                available, capacity = map(int, match.groups())
+                shelter.update(available=available,
+                               capacity=capacity,
+                               last_updated=now(),
+                               last_id=last_id)
+                return
+        # no match, remember the last id
+        shelter.update(last_id=last_id)
 
 @route('/')
 def homepage():
@@ -74,8 +99,18 @@ def server_static(path):
 def api_list():
     return dict(status="ok", data=data)
 
+timer = None
+def update_db():
+    log.debug("update db")
+    for shelter in data:
+        update_shelter_data(shelter)
+    global timer
+    timer = threading.Timer(UPDATE_INTERVAL, update_db)
+    timer.start()
+
 app = bottle.app()
 app.catchall = False
 
+update_db()
 bottle.run(app)
-
+timer.cancel()
