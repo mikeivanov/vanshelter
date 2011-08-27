aadb4441b2cb4f45ec33878b73f0632bd1701cae
Mike Ivanov <mike@mikeivanov.com>
2011-08-27 15:50:13 -0700

    Initial data come from a csv database

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 import re
 import os
-import sys
+import csv
 import bottle
 import logging
 import twitter
@@ -13,69 +13,48 @@ from bottle import route
 
 STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 
-UPDATE_INTERVAL = 10.0
+UPDATE_INTERVAL = 30.0
 
 logging.basicConfig()
 log = logging.getLogger('vanshelter')
 log.setLevel(logging.DEBUG)
 
+tapi = twitter.Api()
+
 def now():
     return datetime.datetime.now().isoformat()
 
-#XXX: make it come from an yaml file
-# our "database"
-data = [dict(id="bcdata_bridge",
-             name="Bridge Women's Emergency Shelter",
-             organization="Atira Women's Resource Society",
-             address=None,
-             phone="604-331-1407",
-             open_from=0,
-             open_until=0,
-             capacity=12,
-             available=0,
-             last_updated=now(),
-             last_id=None,
-             kind="Women",
-             notes=None),
-
-        dict(id="bcdata_st_elizabeth",
-             name="St. Elizabeth Home",
-             organization="St. James Community Services Society",
-             address=None,
-             phone="604-606-0412",
-             open_from=0,
-             open_until=0,
-             capacity=32,
-             available=0,
-             last_updated=now(),
-             last_id=None,
-             kind="Women and their Children",
-             notes=None),
-
-        dict(id="bcdata_lookout",
-             name="Lookout Downtown Shelter",
-             organization="Lookout Emergency Aid Society",
-             address="346 Alexander Street",
-             phone="604-681-9126",
-             open_from=0,
-             open_until=0,
-             capacity=46,
-             available=0,
-             last_updated=now(),
-             last_id=None,
-             kind="Men and Women",
-             notes=None),
-        ]
-
-tapi = twitter.Api()
+def load_data():
+    data = []
+    reader = csv.reader(open('shelters.csv', 'rb'))
+    for id, (twitter, name, org, addr, 
+             phone, capacity, kind) in enumerate(reader):
+        if id > 0:
+            data.append(dict(id=id,
+                             twitter=(None if twitter.strip() == "" 
+                                           else twitter),
+                             name=name,
+                             organization=org,
+                             address=None if addr == 'n/a' else addr,
+                             phone=phone,
+                             open_from=0,
+                             open_until=0,
+                             capacity=int(capacity),
+                             available=0,
+                             last_updated=now(),
+                             last_id=None,
+                             kind=kind))
+    return data
 
 availability_rx = re.compile(r'\[(\d+)/(\d+)\]')
 def update_shelter_data(shelter):
-    statuces = tapi.GetUserTimeline(screen_name="mike_ivanov",
-                                    since_id=shelter['last_id'])
+    #XXX: add error handling
+    statuces = tapi.GetUserTimeline(screen_name=shelter['twitter'],
+                                    since_id=shelter['last_id'],
+                                    count=10)
     if len(statuces) > 0:
         last_id = statuces[0].id
-        for status in reversed(statuces):
+        for status in statuces:
             match = availability_rx.search(status.text)
             if match:
                 available, capacity = map(int, match.groups())
@@ -83,9 +62,23 @@ def update_shelter_data(shelter):
                                capacity=capacity,
                                last_updated=now(),
                                last_id=last_id)
+                log.debug("updated %s" % shelter['id'])
                 return
         # no match, remember the last id
         shelter.update(last_id=last_id)
+
+timer = None
+def update_db():
+    log.debug("update db")
+    for shelter in data:
+        if shelter['twitter']:
+            update_shelter_data(shelter)
+    global timer
+    timer = threading.Timer(UPDATE_INTERVAL, update_db)
+    timer.start()
+
+# our "database"
+data = load_data()
 
 @route('/')
 def homepage():
@@ -99,18 +92,10 @@ def server_static(path):
 def api_list():
     return dict(status="ok", data=data)
 
-timer = None
-def update_db():
-    log.debug("update db")
-    for shelter in data:
-        update_shelter_data(shelter)
-    global timer
-    timer = threading.Timer(UPDATE_INTERVAL, update_db)
-    timer.start()
-
 app = bottle.app()
 app.catchall = False
 
 update_db()
 bottle.run(app)
 timer.cancel()
+
